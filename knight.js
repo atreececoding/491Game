d1611ea4e5b03d8e110a88ae9ac0fb6240d06dee
atreece20@gmail.com <atreece20@gmail.com>
2022-01-19 16:33:36 -0800

    animations

--- knight.js
+++ knight.js
@@ -22,46 +22,42 @@ class Knight {
     };
 
     loadAnimations() {
-        for (var i = 0; i < 6; i++) {
+        for (var i = 0; i < 7; i++) { //states
             this.animations.push([]);
             for (var j = 0; j < 2; j++) {
-                this.animations.push([i]);
+                this.animations.push([i]); //facing left or right
             }
         }
         //idle
         //facing right = 0
-        this.animations[0][0] = new Animator(this.spritesheet, 99, 0, 85, 60, 6, 0.15);
-
+        this.animations[0][0] = new Animator(this.spritesheet, 0, 0, 101, 98, 7, 0.15);
         //facing left = 0
-        this.animations[0][1] = new Animator(this.spritesheet, 99, 0, 99, 60, 6, 0.15);
+        //this.animations[0][1] = new Animator(this.spritesheet, 99, 0, 99, 60, 7, 0.15);
 
         //walking
         //facing right = 0
-        this.animations[1][0] = new Animator(this.spritesheet, 97, 158, 86, 60, 6, 0.15);
-
+        this.animations[1][0] = new Animator(this.spritesheet, 1, 98, 101.5, 61, 7, 0.15);
         //facing left = 0
-        this.animations[1][1] = new Animator(this.spritesheet, 99, 0, 99, 60, 6, 0.15);
+       // this.animations[1][1] = new Animator(this.spritesheet, 99, 0, 99, 60, 6, 0.15);
 
         //Running
         //facing right = 0
-        this.animations[2][0] = new Animator(this.spritesheet, 100, 231, 99, 65, 6, 0.15);
-
+        this.animations[2][0] = new Animator(this.spritesheet, 4, 164, 99, 67, 7, 0.15);
         //facing left = 0
-        this.animations[2][1] = new Animator(this.spritesheet, 99, 0, 99, 65, 6, 0.15);
+       // this.animations[2][1] = new Animator(this.spritesheet, 99, 0, 99, 65, 6, 0.15);
 
         //Jumping
         //facing right = 0
-        this.animations[3][0] = new Animator(this.spritesheet, 106, 323, 97, 90, 6, 0.15);
-
+        list = [110, 202, 284, 382, 480, 587, 703];
+        this.animations[3][0] = new Animator(this.spritesheet, 4, 234, 112, 89, 7, 0.15);
         //facing left = 0
-        this.animations[3][1] = new Animator(this.spritesheet, 99, 0, 99, 90, 6, 0.15);
+       // this.animations[3][1] = new Animator(this.spritesheet, 99, 0, 99, 90, 6, 0.15);
 
         //attacking
         //facing right = 0
-        this.animations[4][0] = new Animator(this.spritesheet, 99, 0, 99, 70, 6, 0.15);
-
+        this.animations[4][0] = new Animator(this.spritesheet, 0, 0, 117, 401, 7, 0.15);
         //facing left = 0
-        this.animations[4][1] = new Animator(this.spritesheet, 99, 0, 99, 70, 6, 0.15);
+       // this.animations[4][1] = new Animator(this.spritesheet, 99, 0, 99, 70, 6, 0.15);
     }
     update() {
         this.x += this.speed * this.game.clockTick;
@@ -69,6 +65,9 @@ class Knight {
     };
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        //this.animations[0][0].drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        //this.animations[1][0].drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        //this.animations[2][0].drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        this.animations[3][0].drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 };
