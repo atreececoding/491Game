495558c75422c3ca160733fb358d4f4451bc798a
Glenn Pak <glennpak159874@gmail.com>
2022-01-26 16:45:21 -0800

    hearts

--- lives.js
+++ lives.js
@@ -1,7 +1,10 @@
 class Lives {
-    constructor(game, lives) {
+    constructor(game) {
         this.game = game;
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/heart.png");
+        this.instanceOfKnight = new Knight();
+        console.log(this.instanceOfKnight.lives);
+        this.lifeCount = (this.instanceOfKnight.lives);
         
 
     };
@@ -10,23 +13,22 @@ class Lives {
     };
     draw(ctx) {
         //TODO: Figure out how to send variable lives to this class in order to update heart count
-
-        //if(lives = 5) {
+        
+        if(this.lifeCount == 5) {
             ctx.drawImage(this.spritesheet, 26 ,33 ,572, 96, 5, 5, 250, 50 )
-        //}
-
-        /*else if (lives = 4) {
+        }
+        else if (this.lifeCount == 4) {
             ctx.drawImage(this.spritesheet, 26 ,145 ,572, 96, 5, 5, 250, 50 )
-        }*/
-        /*else if (lives = 3) {
+        }
+        else if (this.lifeCount ==3) {
             ctx.drawImage(this.spritesheet, 26 ,256 ,572, 96, 5, 5, 250, 50 )
-        }*/
-        /*else if (lives = 2) {
+        }
+        else if (this.lifeCount == 2) {
             ctx.drawImage(this.spritesheet, 26 ,366 ,572, 96, 5, 5, 250, 50 )
-        }*/
-        /*else if ( lives = 1) {
-            //ctx.drawImage(this.spritesheet, 26 ,477 ,572, 96, 5, 5, 250, 50 )
-        }*/
+        }
+        else if (this.lifeCount == 1) {
+            ctx.drawImage(this.spritesheet, 26 ,477 ,572, 96, 5, 5, 250, 50 )
+        }   
     };
 
 };
