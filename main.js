5f48aae5da71e7d1a1a8b27848fc588ee08f3cd2
atreece20@gmail.com <atreece20@gmail.com>
2022-01-29 17:43:47 -0800

    Bat implemented

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ ASSET_MANAGER.queueDownload("./sprites/energydrink.png");
 ASSET_MANAGER.queueDownload("./sprites/heart.png");
 ASSET_MANAGER.queueDownload("./sprites/energy.png");
 ASSET_MANAGER.queueDownload("./sprites/dragon-attack.png");
+ASSET_MANAGER.queueDownload("./sprites/ratAndBat.png");
 
 // Music
 ASSET_MANAGER.queueDownload("./music/AstralAcademy.mp3");
