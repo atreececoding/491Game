85dc39df7b59b8261882caee744e86df02377f46
Seafoam <brancel8@gmail.com>
2022-02-05 22:51:18 -0800

    added in basic functionality for bat and goblin

--- ratAndBat.js
+++ ratAndBat.js
@@ -2,7 +2,7 @@ class RatAndBat {
   constructor(game, x, y, size) {
     Object.assign(this, { game, x, y, size });
 
-    this.velocity = { x: 0, y: 0 };
+    this.velocity = { x: 0, y: -10 };
     this.spritesheet = ASSET_MANAGER.getAsset("./sprites/ratAndBat.png");
 
     this.size = 0;
@@ -49,11 +49,30 @@ class RatAndBat {
 
   die() {}
 
-  update() {}
+  update() {
+    let scale = 1 + Math.random();
+    if (this.x <= 350 && this.facing === 1) {
+      this.velocity.x = 100 * scale;
+      this.facing = 0;
+    } 
+    if (this.x >= 550 && this.facing === 0) {
+      this.velocity.x = -100 * scale;
+      this.facing = 1;
+    }
+    if (this.y <= 30) {
+      this.velocity.y = 50 * scale;
+    } 
+    if (this.y >= 60) {
+      this.velocity.y = -50 * scale;
+    }
+    this.x += this.game.clockTick * this.velocity.x;
+    this.y += this.game.clockTick * this.velocity.y;
+    this.updateBB();
+  }
 
   draw(ctx) {
     const TICK = this.game.clockTick;
-    this.animations[0][0].drawFrame(TICK, ctx, this.x, this.y, 5);
+    this.animations[this.state][0].drawFrame(TICK, ctx, this.x, this.y, 5);
     // ctx.strokeStyle = "Red";
     // ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
   }
